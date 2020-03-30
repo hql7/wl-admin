4418c58735c76c94217b8faa7c57c121001a9117
weilan <17796646068@163.com>
2020-03-30 14:20:10 +0800

    +)

--- src/views/ui/bim/index.vue
+++ src/views/ui/bim/index.vue
@@ -1,6 +1,6 @@
 <template>
   <wl-container>
-    <wl-bim-viewer multiple :docs="bims" class="wl-viewer"></wl-bim-viewer>
+    <wl-bim-viewer :docs="bims" class="wl-viewer"></wl-bim-viewer>
   </wl-container>
 </template>
 
@@ -12,32 +12,7 @@ export default {
       bims: [
         {
           name: "urn_model1",
-          path: "bim/1/0.svf",
-          modelObj: null
-        },
-        {
-          name: "urn_model2",
-          path: "bim/2/0.svf",
-          modelObj: null
-        },
-        {
-          name: "urn_model3",
-          path: "bim/3/0.svf",
-          modelObj: null
-        },
-        {
-          name: "urn_model4",
-          path: "bim/4/0.svf",
-          modelObj: null
-        },
-        {
-          name: "urn_model5",
-          path: "bim/6/Sphere.svf",
-          modelObj: null
-        },
-        {
-          name: "urn_model6",
-          path: "bim/6/Sphere.svf",
+          path: "http://wlui.oss-cn-beijing.aliyuncs.com/bimdata/0.pf",
           modelObj: null
         }
       ]
--- vue.config.js
+++ vue.config.js
@@ -3,23 +3,16 @@
 let styleVariables = require('./src/style/variables.scss.js');
 
 module.exports = {
-  // 将资源打包为相对路径
-  // publicPath: "././",
-  crossorigin: 'anonymous', // htmlWebpackPlugin
+  /* crossorigin: 'anonymous', // htmlWebpackPlugin
   devServer: {
-    // host: '0.0.0.0',
     hot: true,
     open: true, // 自动调用默认浏览器打开
     disableHostCheck: true,
-    // port,
     overlay: {
       warnings: false,
       errors: true
     },
-    /* headers: {
-      'Access-Control-Allow-Origin': '*',
-    } */
-  },
+  }, */
   css: {
     loaderOptions: {
       sass: {
